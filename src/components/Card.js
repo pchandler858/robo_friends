dd3f76b575316eec37d3f985d2eb60fdf29bb3a7
Preston C <pchandler858@gmail.com>
2022-12-23 16:56:58 -0800

    fixed card size descrepancy

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Card = ({ name, email, id }) => {
   return (
-    <div className="tc bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5">
-      <img src={`https://robohash.org/${id}?size=200x200`} alt="robots" />
+    <div className="tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5 ">
+      <img src={`https://robohash.org/${id}?size=265x265`} alt="robots" />
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
